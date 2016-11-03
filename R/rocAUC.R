52e03347540c51e8a68435147cdf7fbad5f203f7
JAQuent <alexander.quent@rub.de>
2016-11-03 16:03:55 +0100

    Add files via upload

--- R/rocAUC.R
+++ R/rocAUC.R
@@ -1,7 +1,7 @@
 #' Caculation of Area Under the Curve (AUC) for an ROC plot
 #'
 #' This function calculates the AUC by summing the tri- and rectangles, which can be made of the ROC points. 
-#' If missing, the y-intercept is added by linear interpolation. The last point, where both false Alarm rates reach 1, is also added. 
+#' If missing, the y-intercept is added by linear interpolation. The last point, where both rates reach 1, is also added. 
 #' @author Joern Alexander Quent, \email{alexander.quent@rub.de}
 #' @param falseAlarm A vector containing the false alarm rate.
 #' @param hit A vector containing the hit rate.
@@ -11,14 +11,19 @@
 
 rocAUC <- function(falseAlarm, hit){
   # Adding a y-intercept by linear interpolation
-  if (min(falseAlarm) != 0){
-    linear_slope     <- (hit[2] - hit[1])/(falseAlarm[2] - falseAlarm[1])
-    linear_intercept <- hit[1] - linear_slope * falseAlarm[1]
+  if(falseAlarm[1] != falseAlarm[2]){ # Checking whether the first two values are repeating.
+    if (min(falseAlarm) != 0){ # If missing, the y-intercept is interpolated.
+      linear_slope     <- (hit[2] - hit[1])/(falseAlarm[2] - falseAlarm[1])
+      linear_intercept <- hit[1] - linear_slope * falseAlarm[1]
+      falseAlarm <- c(0, falseAlarm, 1)
+      hit        <- c(linear_intercept, hit, 1)
+    } else {
+      falseAlarm <- c(falseAlarm, 1)
+      hit        <- c(hit, 1)
+    }
+  } else { # If it repeats, then use this method. 
     falseAlarm <- c(0, falseAlarm, 1)
-    hit        <- c(linear_intercept, hit, 1)
-  } else {
-    falseAlarm <- c(falseAlarm, 1)
-    hit        <- c(hit, 1)
+    hit        <- c(0, hit, 1)
   }
   
   # Calculating AUC by summing the areas of triangles and rectangles
